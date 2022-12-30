5ef1f35e569914fa60284a1e1e266d0765733bec
hamzahhisham306 <hamzahhisham306@gmail.com>
2022-12-30 21:22:25 +0000

    main

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FaAward } from 'react-icons/fa';
+import { VscFolderLibrary } from 'react-icons/vsc';
+import ME from '../../assets/hamza.jfif';
+import './intro.css';
+
+const Intro = () => {
+  return (
+    <section id="about">
+      <h5>Get to know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>1 year</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>20+ Completed Projects</small>
+            </article>
+          </div>
+          <p>Full Stack Web Developer with a Background in Cyber Security. Passionate about using my 
+knowledge to maximize efficiency while developing creative, customer focused solutions.
+Fluent in Arabic and have a professional level in English.</p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Intro
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,158 @@
+import React from 'react';
+import IMG1 from '../../assets/app2.png';
+import IMG2 from '../../assets/app1.png';
+import IMG3 from '../../assets/app5.png';
+import IMG4 from '../../assets/app4.png';
+import IMG5 from '../../assets/app6.png';
+import IMG6 from '../../assets/app7.png';
+import IMG7 from '../../assets/app8.png';
+import IMG9 from '../../assets/app9.png';
+import IMG10 from '../../assets/app10.png';
+
+import './portfolio.css';
+
+const Portfolio = () => {
+  const soloProjects = [
+    {
+      id: 8,
+      title: 'Design Studio',
+      img: '',
+      description:
+        'We have built a website for you that provides the ability to choose your own styling and have it printed on our great quality T-shirt.',
+      technologies: 'React | CSS | Node.js | Express.js | MongoDB',
+      link: 'https://github.com/orgs/Black-Ops-Geekss/repositories',
+      github: 'https://github.com/orgs/Black-Ops-Geekss/repositories',
+    },
+    {
+      id: 9,
+      title: 'JOurney',
+      img: IMG9,
+      description:
+        'we have built a website for schools that provides Organizing fun, useful and safe school trips takes a lot of time and effort and requires special study for the trip ',
+      technologies: 'React | Redux | CSS | Node.js | Express.js | PostgreSQL',
+      link: 'https://github.com/orgs/School-trip-app/repositories',
+      github: 'https://github.com/orgs/School-trip-app/repositories',
+    },
+    {
+      id: 10,
+      title: 'Typing Speed',
+      img: IMG10,
+      description:
+        'Typing Speed Test Game',
+      technologies: 'html | CSS | javascript',
+      link: 'https://hamzahhisham306.github.io/Typeing/',
+      github: 'https://github.com/hamzahhisham306/Typeing',
+    },
+    {
+      id: 1,
+      title: 'GPT-3',
+      img: IMG1,
+      description:
+        'GPT-3 :project-modern-gpt3 ',
+      technologies: 'React | CSS ',
+      link: 'https://timely-cajeta-a18b15.netlify.app/',
+      github: 'https://github.com/hamzahhisham306/GPT-3-ReactJS',
+    },
+    {
+      id: 2,
+      title: 'restairant-design',
+      img: IMG2,
+      description:
+        'restairant-design  app that provides information about available products and their details',
+      technologies: 'React | CSS',
+      link: 'https://sparkling-beignet-214171.netlify.app/',
+      github: 'https://github.com/hamzahhisham306/restairant-design',
+    },
+    {
+      id: 3,
+      title: 'To-Do-App',
+      img: IMG4,
+      description: 'User friendly app to plan your day and manage tasks',
+      technologies: 'html | CSS | javascript',
+      link: 'https://hamzahhisham306.github.io/todo/',
+      github: 'https://github.com/hamzahhisham306/todo',
+    },
+    {
+      id: 4,
+      title: 'templet3',
+      img: IMG3,
+      description:
+        'Fully responsive interactive website built based on Figma design',
+      technologies: 'HTML | CSS',
+      link: 'https://hamzahhisham306.github.io/templet3/',
+      github: 'https://github.com/hamzahhisham306/templet3',
+    },
+    {
+      id: 5,
+      title: 'Kasper',
+      img: IMG5,
+      description:
+        'Fully responsive interactive website built based on Adobe XD design',
+      technologies: 'HTML | CSS',
+      link: 'https://hamzahhisham306.github.io/kasper/',
+      github: 'https://github.com/hamzahhisham306/kasper',
+    },
+    {
+      id: 6,
+      title: 'Python-game',
+      img: IMG6,
+      description:
+        'python game ',
+      technologies: ' HTML  | CSS | JavaScript',
+      link: 'https://hamzahhisham306.github.io/python-game/',
+      github: 'https://github.com/hamzahhisham306/python-game',
+    },
+    {
+      id: 7,
+      title: 'Burger website',
+      img: IMG7,
+      description:
+        'Burger website',
+      technologies: ' HTML  | CSS | JavaScript',
+      link: 'https://hamzahhisham306.github.io/Bruger/',
+      github: 'https://github.com/hamzahhisham306/Bruger',
+    },
+  ];
+
+  return (
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {soloProjects.map((pro) => (
+          <article className="portfolio__item" key={pro.id}>
+            <div className="portfolio__item-image">
+              <img src={pro.img} alt={pro.title} />
+            </div>
+            <div className="portfolio__item-content">
+              <h3>{pro.title}</h3>
+              <p>{pro.description}</p>
+              <p>{pro.technologies}</p>
+            </div>
+            <div className="portfolio__item-cta">
+              <a
+                href={pro.github}
+                target="_blank"
+                className="btn"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href={pro.link}
+                target="_blank"
+                className="btn btn-primary"
+                rel="noreferrer"
+              >
+                Visit Website
+              </a>
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
