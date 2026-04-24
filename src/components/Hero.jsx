842e90c9e72606ba6c35601fe2c3e9b9fd307eab
Hamzah Aldaamas <86842077+hamzahhisham306@users.noreply.github.com>
2026-04-24 14:32:53 +0300

    sdfsd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,14 +32,14 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 z-10`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 z-10 pointer-events-none`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
-        <div>
+        <div className='pointer-events-auto'>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className='text-[#915EFF]'>Hamzah</span>
           </h1>
@@ -100,10 +100,12 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      <div className='absolute top-[280px] xs:top-[220px] sm:top-0 left-0 right-0 bottom-0'>
+        <ComputersCanvas />
+      </div>
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-        <a href='#about'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center pointer-events-none'>
+        <a href='#about' className='pointer-events-auto'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
